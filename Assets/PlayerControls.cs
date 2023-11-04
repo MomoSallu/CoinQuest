d56de8372849eff96ee43bcff7d83beff9cbcd64
MomoSallu <muhammadsallu@gmail.com>
2023-11-04 21:01:09 +0000

    Fixed animations & added groundCheck to help

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    private Animator animator;
     public float jumpStrength = 5f;
     public float movementSpeed = 4f;
-    Rigidbody rb;   
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundLayer;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponentInParent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,9 +26,24 @@ public class PlayerControls : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
-        
-        if (Input.GetButtonDown("Jump")) { // refers to the Input Manager in Project Settings
-            rb.velocity = new Vector3( rb.velocity.x, jumpStrength, rb.velocity.y );
+        animator.SetFloat("moveX", horizontalInput);
+        animator.SetFloat("moveY", verticalInput);
+
+        /* if (rb.velocity.magnitude > 0)
+        {
+            animator.SetBool("isMoving", true);
         }
+        else { animator.SetBool("isMoving", false); } */
+        
+        // refers to the Input Manager in Project Settings
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpStrength, rb.velocity.y);
+        }
+        animator.SetBool("isJumping", !IsGrounded());
+        animator.SetBool("isFalling", !IsGrounded() && groundCheck.position.y < -1.5);
+    }
+    bool IsGrounded() {
+         return Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
     }
 }
