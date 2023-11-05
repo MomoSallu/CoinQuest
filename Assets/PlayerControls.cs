cb476f6fea13fbdb5edee9c64d44e84364832e7d
MomoSallu <muhammadsallu@gmail.com>
2023-11-05 16:19:47 +0000

    Improved jump animations

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -24,26 +24,35 @@ public class PlayerControls : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
         animator.SetFloat("moveX", horizontalInput);
         animator.SetFloat("moveY", verticalInput);
 
-        /* if (rb.velocity.magnitude > 0)
-        {
-            animator.SetBool("isMoving", true);
-        }
-        else { animator.SetBool("isMoving", false); } */
-        
-        // refers to the Input Manager in Project Settings
+         // refers to the Input Manager in Project Settings
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpStrength, rb.velocity.y);
         }
-        animator.SetBool("isJumping", !IsGrounded());
-        animator.SetBool("isFalling", !IsGrounded() && groundCheck.position.y < -1.5);
+        
+        if (!IsGrounded() && rb.velocity.y > 0) {
+            animator.SetBool("isJumping", true);
+        } else if(IsGrounded() && rb.velocity.y < 0){
+            animator.SetBool("isJumping", false);
+        }
+        if (!IsGrounded() && rb.velocity.y < 0) {
+            animator.SetBool("isFalling", true);
+        } else {
+            animator.SetBool("isFalling", false);
+        }
     }
     bool IsGrounded() {
-         return Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
-    }
+        bool grounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+        if (grounded)
+        {
+            animator.SetBool("hasFinishedJumping", true);
+        }
+        else { animator.SetBool("hasFinishedJumping", false); }
+        animator.SetBool("isGrounded", grounded);
+        return grounded;
+     }
 }
