fcd1c7d1788acd3e58ff71743337a13b203f43e9
MomoSallu <muhammadsallu@gmail.com>
2023-11-09 16:27:19 +0000

    Added an enemy
    Added an enemy that follows waypoints and rotates when those waypoints are reached.

--- Assets/WaypointFollower.cs
+++ Assets/WaypointFollower.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class WaypointFollower : MonoBehaviour
 {
+    
     [SerializeField] GameObject[] waypoints;
     [SerializeField] float speed = 1f;
     int currentWaypoint = 0;
+    public GameObject enemy;
+    float rotateSpeed = 1f;
+    private void Start()
+    {
+    
+    }
     void Update()
     {
         if (Vector3.Distance(transform.position, waypoints[currentWaypoint].transform.position) < 0.1f) {
@@ -17,5 +25,14 @@ public class WaypointFollower : MonoBehaviour
             }
         }
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+
+        
+        if (GameObject.FindGameObjectWithTag("Enemy"))
+        {
+            Vector3 waypointPos = waypoints[currentWaypoint].transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(waypointPos - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed);
+        }
+
     }
 }
